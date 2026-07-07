20bbad4c9a305f6dc671d81bc20ad552d16af63c
Kalhara Ediriweera <137047140+kalhara-ediriweera@users.noreply.github.com>
2026-07-08 03:15:45 +0530

    Update MongoDB connection URI for production

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/sparex');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://kavindukalha:Kavindu17@cluster0.vygr9.mongodb.net/sparex?retryWrites=true&w=majority');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
